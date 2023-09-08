56461eb7679eef7c04b1ce4610ae8f07d36e1463
CriminalChicken <y.meichtry@gmx.ch>
2023-09-08 16:29:44 +0200

    In shiftoverview shows the input from enterplayers

--- EnterPlayers/enterplayers.js
+++ EnterPlayers/enterplayers.js
@@ -1,56 +1,54 @@
-// This function runs only when the DOM is fully loaded.
 document.addEventListener("DOMContentLoaded", function () {
+    // Funktion zum Speichern der Spielerdaten
+    function savePlayerData() {
+        const playerData = [];
+        const playerListItems = document.querySelectorAll(".player-list input");
 
-    // JavaScript function to add another input field
+        playerListItems.forEach(function (item) {
+            if (item.value.trim() !== '') { // Überprüfe, ob das Feld nicht leer ist
+                playerData.push(item.value);
+            }
+        });
+
+        // Speichere die Spielerdaten im Local Storage
+        localStorage.setItem('playerData', JSON.stringify(playerData));
+    }
+
+    // Event-Listener für den "Next ->" Link
+    document.getElementById("nextButton").addEventListener("click", function () {
+        savePlayerData();
+        window.location.href = "../ShiftOverview/shiftoverview.html";
+    });
+
+    // JavaScript-Funktion, um ein weiteres Input-Feld hinzuzufügen
     document.getElementById("addPlayerInput").addEventListener("click", function () {
-        console.log("plus input"); // Log message
+        console.log("plus input"); // Log-Meldung
 
         const playerList = document.getElementById("playerList");
 
-        // Create a new list item and an input field
+        // Erstellen Sie ein neues Listenelement und ein Input-Feld
         const listItem = document.createElement("li");
         const input = document.createElement("input");
 
-        // Set properties of the input field
+        // Setzen Sie die Eigenschaften des Input-Felds
         input.type = "text";
         input.placeholder = "Player " + (playerList.childElementCount + 1);
 
-        // Add the input field to the list item and the list item to the player list
+        // Fügen Sie das Input-Feld dem Listenelement hinzu und das Listenelement der Spielerliste
         listItem.appendChild(input);
         playerList.appendChild(listItem);
     });
 
-    // JavaScript function to remove an input field
+    // JavaScript-Funktion, um ein Input-Feld zu entfernen
     document.getElementById("deletePlayerInput").addEventListener("click", function () {
-        console.log("minus input"); // Log message
+        console.log("minus input"); // Log-Meldung
 
         const playerList = document.getElementById("playerList");
         const lastItem = playerList.lastElementChild;
 
-        // Check if there is at least one input field before removing one
+        // Überprüfen Sie, ob es noch mindestens ein Input-Feld gibt, bevor Sie eins entfernen
         if (playerList.childElementCount > 1) {
             playerList.removeChild(lastItem);
         }
     });
-});
-
-// Funktion zum Weiterleiten an die nächste Seite
-document.getElementById("nextButton").addEventListener("click", function () {
-    // Holen Sie sich die eingegebenen Spieler und konvertieren Sie sie in ein Array
-    const players = [];
-    for (let i = 1; i <= 10; i++) {
-        const playerName = document.querySelector(`#player${i}`).value;
-        if (playerName.trim() !== '') {
-            players.push(playerName);
-        }
-    }
-
-    // Konvertieren Sie das Array in einen JSON-String
-    const playersJSON = JSON.stringify(players);
-
-    // Speichern Sie den JSON-String im versteckten Feld
-    document.getElementById("playerList").value = playersJSON;
-
-    // Navigieren Sie zur nächsten Seite
-    window.location.href = "../ShiftOverview/shiftoverview.html";
 });
--- ShiftOverview/shiftoverview.js
+++ ShiftOverview/shiftoverview.js
@@ -1,41 +1,32 @@
+// JavaScript-Funktion zum Laden der Spielerdaten aus dem Local Storage
+function loadPlayerData() {
+    const playerData = JSON.parse(localStorage.getItem('playerData'));
+    return playerData || []; // Rückgabe der gespeicherten Daten oder eines leeren Arrays
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-    // Holen Sie sich die übergebene Anzahl der Spieler pro Schicht von Enter Players
-    const playersPerShift = parseInt(sessionStorage.getItem("playersPerShift"));
-
-    // Holen Sie sich die übergebene Spielerliste von Enter Players
-    const players = JSON.parse(sessionStorage.getItem("playerList"));
-
-    // Holen Sie sich den Container für die Schichten
     const shiftsContainer = document.getElementById("shiftsContainer");
+    const playerData = loadPlayerData(); // Laden der Spielerdaten
+    const playersPerShift = 3; // Hier die Anzahl der Spieler pro Schicht einstellen
 
-    // Überprüfen Sie, ob sowohl playersPerShift als auch players vorhanden sind
-    if (playersPerShift && players) {
-        // Berechnen Sie die Anzahl der Schichten basierend auf der Spieleranzahl
-        const totalShifts = Math.ceil(players.length / playersPerShift);
+    // Funktion zum Generieren der Schichten
+    function generateShifts() {
+        shiftsContainer.innerHTML = ''; // Löschen Sie den Inhalt des Containers
 
-        // Schleife durch die Schichten
-        for (let shift = 0; shift < totalShifts; shift++) {
-            // Erstellen Sie ein neues Div für jede Schicht
-            const shiftDiv = document.createElement("div");
-            shiftDiv.classList.add("shift");
+        for (let i = 0; i < playerData.length; i += playersPerShift) {
+            const shift = document.createElement("div");
+            shift.classList.add("shift");
 
-            // Erstellen Sie eine Überschrift für die Schicht
-            const shiftHeading = document.createElement("h3");
-            shiftHeading.textContent = `Shift ${shift + 1}`;
-            shiftDiv.appendChild(shiftHeading);
-
-            // Erstellen Sie eine Liste für die Spieler in dieser Schicht
-            const playerList = document.createElement("ul");
-
-            // Schleife durch die Spieler in dieser Schicht
-            for (let i = shift * playersPerShift; i < (shift + 1) * playersPerShift && i < players.length; i++) {
-                const playerItem = document.createElement("li");
-                playerItem.textContent = players[i];
-                playerList.appendChild(playerItem);
+            for (let j = i; j < i + playersPerShift && j < playerData.length; j++) {
+                const player = document.createElement("p");
+                player.textContent = `${j + 1}. ${playerData[j]}`;
+                shift.appendChild(player);
             }
 
-            shiftDiv.appendChild(playerList);
-            shiftsContainer.appendChild(shiftDiv);
+            shiftsContainer.appendChild(shift);
         }
     }
+
+    // Rufen Sie die Funktion auf, um die Schichten zu generieren
+    generateShifts();
 });
