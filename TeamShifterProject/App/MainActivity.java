901df11e165036db09b216ad6c7963e76d2ed156
CriminalChicken <y.meichtry@gmx.ch>
2023-11-09 09:55:15 +0100

    new files for the app

---
+++ TeamShifterProject/App/MainActivity.java
@@ -0,0 +1,19 @@
+package AppTester;
+  
+import androidx.appcompat.app.AppCompatActivity; 
+  
+import android.os.Bundle; 
+import android.webkit.WebView; 
+  
+public class MainActivity extends AppCompatActivity { 
+  
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { 
+        super.onCreate(savedInstanceState); 
+        setContentView(R.layout.activity_main); 
+  
+        WebView webView=findViewById(R.id.webView); 
+        webView.getSettings().setJavaScriptEnabled(true); 
+        webView.loadUrl("file:///android_asset/index.html"); 
+    } 
+}
